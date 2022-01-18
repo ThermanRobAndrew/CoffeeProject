f62ad6c05fb157633e5a437bd78c3cd2b4333429
Andrew Lange <andrew.lange13@gmail.com>
2022-01-18 10:18:50 -0600

    adds local storage again

--- main.js
+++ main.js
@@ -86,10 +86,13 @@ function addACoffee(e) {
         roast: newRoastSelection.value
     };
     coffees.push(addedCoffee);
+    localStorage.setItem("newCoffeeObj",JSON.stringify(addedCoffee))
+    console.log(localStorage)
     tbody.innerHTML = renderCoffees(coffees);
 }
 
-
+let storedCoffee = JSON.parse(localStorage.getItem("newCoffeeObj"))
+coffees.push(storedCoffee)
 
 
 /// DOM Variables
